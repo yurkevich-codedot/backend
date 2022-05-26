c6cbe4d2af9d3848d8d2652933ad3558201c51f9
DaniilYurkevich <daniilsergeevich200111@gmail.com>
2022-05-26 20:31:02 +0300

    make so more things

--- dist/content.php
+++ dist/content.php
@@ -43,49 +43,50 @@ require ('./header.php');
           foreach($titles as $title)
           {
             echo '<div class="content__items swiper">
-            <h1 class="main__title--padding-bottom">'.$title[1].'</h1>
-            <div class="swiper-wrapper swiper-flex">
-          ';
-              foreach($info as $item)
-              {
-                  if($title[1]==$item[3])
-                  {
-                    $file = $_SERVER['DOCUMENT_ROOT']."/dist/img/uploads/attractions/$item[0]/main.png";
-                    $file_exists = file_exists($file);
-                    echo '
-                      <div class="content__item swiper-slide">';
-                      if($file_exists)
-                      {
-                        echo'
-                          <div class="main__img-wrapper">
-                            <img
-                              src="./img/uploads/attractions/'.$item[0].'/main.png"
-                              class="main__img"
-                              alt="'.$item[0].'"
-                            />
-                          </div>
-                          ';
-                      }
-                          echo'
-                          <div class="content__name-wrapper">
-                            <div class="content__name">'.$item[1].'</div>
-                            <div class="content__name">'.$item[2].'</div>
-                            <div class="content__name">'.$item[4].'</div>
-                            <a href="/dist/attraction.php?id='.$item[0].'" class="content__btn" tabindex="-1"
-                              >Подробнее</a
-                            >
-                          </div>
-                          </div>';
-                  }
-                }
+                          <h1 class="main__title--padding-bottom">'.$title[1].'</h1>
+                            <div class="swiper-wrapper swiper-flex">';
+                            foreach($info as $item)
+                            {
+                                if($title[1]==$item[3])
+                                {
+                                  $file = $_SERVER['DOCUMENT_ROOT']."/dist/img/uploads/attractions/$item[0]/main.png";
+                                  $file_exists = file_exists($file);
+                                  echo '
+                                    <div class="content__item swiper-slide">';
+                                    if($file_exists)
+                                    {
+                                      echo'
+                                        <div class="main__img-wrapper">
+                                          <img
+                                            src="./img/uploads/attractions/'.$item[0].'/main.png"
+                                            class="main__img"
+                                            alt="'.$item[0].'"
+                                          />
+                                        </div>
+                                        ';
+                                    }
+                                        echo'
+                                        <div class="content__name-wrapper">
+                                          <div class="content__name">'.$item[1].'</div>
+                                          <div class="content__name">'.$item[2].'</div>
+                                          <div class="content__name">'.$item[4].'</div>
+                                          <a href="/dist/attraction.php?id='.$item[0].'" class="content__btn" tabindex="-1"
+                                            >Подробнее</a
+                                          >
+                                        </div>
+                                        </div>';
+                                }
+                              }
                 echo'
-                  <div class="swiper-pagination"></div>
-                  <div class="prev"></div>
-                  <div class="next"></div>
-                  <div class="swiper-scrollbar"></div>
                   </div>
                 ';
+                echo '<div class="swiper-pagination"></div>
+          <div class="prev"></div>
+        <div class="next"></div>
+       <div class="swiper-scrollbar"></div>
+     </div>';
           }
+          
           ?>
         </div>
    </div>
--- dist/footer-block.php
+++ dist/footer-block.php
@@ -1,7 +1,13 @@
 <?
 require("php/scripts/connect.php");
-$sql = mysqli_query($mysqli,"SELECT * FROM `news` ORDER BY `news`.`id` DESC LIMIT 4");
-$news = mysqli_fetch_all($sql);
+$sqlNews = mysqli_query($mysqli,"SELECT * FROM `news` ORDER BY `news`.`id` DESC LIMIT 4");
+$sqlLocality = mysqli_query($mysqli,"SELECT * FROM `locality` ORDER BY RAND() LIMIT 4");
+$sqlAttraction = mysqli_query($mysqli,"SELECT * FROM `attraction` ORDER BY RAND() LIMIT 4");
+
+
+$news = mysqli_fetch_all($sqlNews);
+$locality = mysqli_fetch_all($sqlLocality);
+$attarction = mysqli_fetch_all($sqlAttraction);
 
 ?>
 
@@ -45,31 +51,22 @@ $news = mysqli_fetch_all($sql);
             </div>
           </div>
           <div class="footer__wrapper-inner">
-            <div class="footer__list-wrapper">
-              <a href="content.html" class="footer__list-title"
-                >Достопримечательности</a
-              >
+          <div class="footer__list-wrapper">
+            <a href="content.php" class="footer__list-title">Достопримечательности</a>
               <div class="footer__items">
-                <a href="attraction.html" class="footer__item">Три штыка</a>
-                <a href="attraction.html" class="footer__item"
-                  >Памятник Князю Альгерду</a
-                >
-                <a href="attraction.html" class="footer__item"
-                  >Памятник Воинам-интернационалистам</a
-                >
-                <a href="attraction.html" class="footer__item">Три штыка</a>
+              <? 
+              foreach($attarction as $item)
+              {
+              echo'
+            
+                <a href="attraction.php?id='.$item[0].'" class="footer__item"
+                  >'.$item[1].'</a
+                >';}
+                ?>
               </div>
             </div>
             <div class="footer__list-wrapper">
-              <a href="#" class="footer__list-title">Категории</a>
-              <div class="footer__items">
-                <a href="#" class="footer__item">Памятники</a>
-                <a href="#" class="footer__item">Исторический объект</a>
-                <a href="#" class="footer__item">Культурный объект</a>
-              </div>
-            </div>
-            <div class="footer__list-wrapper">
-            <a href="news.html" class="footer__list-title">Новости</a>
+            <a href="news.php?page=1" class="footer__list-title">Новости</a>
               <div class="footer__items">
               <? 
               foreach($news as $item)
@@ -85,10 +82,11 @@ $news = mysqli_fetch_all($sql);
             <div class="footer__list-wrapper">
               <a href="#" class="footer__list-title">Населенный пункт</a>
               <div class="footer__items">
-                <a href="#" class="footer__item">Витебск</a>
-                <a href="#" class="footer__item">Толочин</a>
-                <a href="#" class="footer__item">Сенно</a>
-                <a href="#" class="footer__item">Ушачи</a>
+                <?
+                foreach($locality as $item) 
+                echo'
+                <a href="#" class="footer__item">'.$item[1].'</a>';
+                ?>
               </div>
             </div>
           </div>
